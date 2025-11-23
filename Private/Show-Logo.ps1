dfec7469a3eb5424b1d15350d207a1bc88ebd7e8
Jake Hildreth <jakehildreth@gmail.com>
2025-11-23 14:00:13 -0800

    feat: new logo

--- Private/Show-Logo.ps1
+++ Private/Show-Logo.ps1
@@ -1,20 +1,22 @@
 ﻿function Show-Logo {
     param (
-        [string]$Version = (Get-Date -Format yyyy.M.d.Hmm)
+        [string]$Version = (Get-Date -Format yyyy.M.d.Hmm),
+        [int]$padpad = 11
     )
     Write-Host
     $logo = @"
-██     ▄████▄ ▄█████ ██ ▄█▀ ▄█████ ██▄  ▄██ ██ ██████ ██  ██ ██    ▀██
-██     ██  ██ ██     ████   ▀▀▀▄▄▄ ██ ▀▀ ██ ██   ██   ██████ ███▀  ▄██
-██████ ▀████▀ ▀█████ ██ ▀█▄ █████▀ ██    ██ ██   ██   ██  ██ ██   ▀▀██
-"@
-    
+█████████████████████████████▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀█████████
+█  █████▀    ▀█▀     █  █▀ ▄█ ▄█████ ██▄  ▄██ ██ ██████ ██  ██ ██    ▀██
+█  █████  ██  █  █████    ███ ▀▀▀▄▄▄ ██ ▀▀ ██ ██   ██   ██████ ███▀  ▄██
+█      █▄    ▄█▄     █  █▄ ▀█ █████▀ ██    ██ ██   ██   ██  ██ ██   ▀▀██
+█████████████████████████████▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄█████████
+"@  
     Write-Host $logo -ForegroundColor DarkGray -BackgroundColor Black
     
     # Right-align version string, ending 10 characters before the end
     $logoWidth = ($logo -split "`n")[0].Length
-    $padding = [Math]::Max(0, $logoWidth - $Version.Length - 11)
-    $rightAlignedVersion = (' ' * $padding) + $Version + (' ' * 11)
+    $padding = [Math]::Max(0, $logoWidth - $Version.Length - $padpad)
+    $rightAlignedVersion = (' ' * $padding) + $Version + (' ' * $padpad)
     
     Write-Host $rightAlignedVersion -ForegroundColor DarkGray -BackgroundColor Black
 }
