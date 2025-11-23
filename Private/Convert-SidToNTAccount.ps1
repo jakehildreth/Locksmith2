533fe24e0772b2f730e67f2a3c793fb52d90e7c7
Jake Hildreth <jakehildreth@gmail.com>
2025-11-23 07:10:26 -0800

    fix: comment out unused code in Convert-SidToNTAccount.ps1 for clarity

--- Private/Convert-SidToNTAccount.ps1
+++ Private/Convert-SidToNTAccount.ps1
@@ -1,11 +1,11 @@
-$sid = 'S-1-5-21-2298154379-2017097123-1132854487-519'
-$searchBase = $RootDSE.Parent
-$searcherDirectoryEntry = New-Object System.DirectoryServices.DirectoryEntry(
-    $searchBase,
-    $Credential.UserName,
-    $Credential.GetNetworkCredential().Password
-)
-$searcher = New-Object System.DirectoryServices.DirectorySearcher($searcherDirectoryEntry)
-$searcher.Filter = "(objectSid=$sid)"
-$result = $searcher.FindOne()
-$ntAccountName = $result.Properties['sAMAccountName'][0]
+# $sid = 'S-1-5-21-2298154379-2017097123-1132854487-519'
+# $searchBase = $RootDSE.Parent
+# $searcherDirectoryEntry = New-Object System.DirectoryServices.DirectoryEntry(
+#     $searchBase,
+#     $Credential.UserName,
+#     $Credential.GetNetworkCredential().Password
+# )
+# $searcher = New-Object System.DirectoryServices.DirectorySearcher($searcherDirectoryEntry)
+# $searcher.Filter = "(objectSid=$sid)"
+# $result = $searcher.FindOne()
+# $ntAccountName = $result.Properties['sAMAccountName'][0]
