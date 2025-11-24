5c675ea717efd0d0f83f4ed8ee558a6ad3310813
Jake Hildreth <jakehildreth@gmail.com>
2025-11-23 16:39:48 -0800

    fix: enhance Show-Logo function with customizable colors

--- Private/Show-Logo.ps1
+++ Private/Show-Logo.ps1
@@ -1,22 +1,30 @@
 ﻿function Show-Logo {
     param (
         [string]$Version = (Get-Date -Format yyyy.M.d.Hmm),
-        [int]$padpad = 11
+        [int]$padpad = 11,
+        [ValidateScript({ [enum]::GetValues([System.ConsoleColor]) -contains $_ })]
+        [string]$ForegroundColor = 'DarkGray',
+        [ValidateScript({ [enum]::GetValues([System.ConsoleColor]) -contains $_ })]
+        [string]$BackgroundColor = 'Black'
     )
+
     Write-Host
-    $logo = @"
-██████████████████████████████▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀█████████
-██  █████▀    ▀█▀     █  █▀ ▄█ ▄█████ ██▄  ▄██ ██ ██████ ██  ██ ██    ▀██
-██  █████  ██  █  █████    ███ ▀▀▀▄▄▄ ██ ▀▀ ██ ██   ██   ██████ ███▀  ▄██
-██      █▄    ▄█▄     █  █▄ ▀█ █████▀ ██    ██ ██   ██   ██  ██ ██   ▀▀██
-██████████████████████████████▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄█████████
-"@  
-    Write-Host $logo -ForegroundColor DarkGray -BackgroundColor Black
+    $logo = @(
+        '█▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀█████████',
+        '█ ██     ▄████▄ ▄█████ ██ ▄█▀ ▄█████ ██▄  ▄██ ██ ██████ ██  ██ ██    ▀██',
+        '█ ██     ██  ██ ██     ████   ▀▀▀▄▄▄ ██ ▀▀ ██ ██   ██   ██████ ███▀  ▄██',
+        '█ ██████ ▀████▀ ▀█████ ██ ▀█▄ █████▀ ██    ██ ██   ██   ██  ██ ██   ▀▀██',
+        '█▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄█████████'
+    )
+
+    $logo | ForEach-Object {
+        Write-Host $_ -ForegroundColor $ForegroundColor -BackgroundColor $BackgroundColor -NoNewline; Write-Host
+    }
     
     # Right-align version string, ending 10 characters before the end
     $logoWidth = ($logo -split "`n")[0].Length
     $padding = [Math]::Max(0, $logoWidth - $Version.Length - $padpad)
     $rightAlignedVersion = (' ' * $padding) + $Version + (' ' * $padpad)
     
-    Write-Host $rightAlignedVersion -ForegroundColor DarkGray -BackgroundColor Black
+    Write-Host $rightAlignedVersion -ForegroundColor $ForegroundColor
 }
