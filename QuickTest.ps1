eab96ed09371a320cd2fc62f6b9873a71b23f459
Jake Hildreth <jakehildreth@gmail.com>
2025-11-28 19:23:31 -0800

    refactor: remove unused variables and streamline Invoke-Locksmith2 call

--- QuickTest.ps1
+++ QuickTest.ps1
@@ -2,6 +2,4 @@ Set-Location ~\Documents\Locksmith2\
 Import-Module .\Locksmith2.psd1 -Force
 $Credential = New-Credential -User 'adcs.goat\Administrator'
 $Forest = 'adcs.goat'
-$RootDSE = Get-RootDSE -Forest $Forest -Credential $Credential
-$AdcsObject = Get-AdcsObject -RootDSE $RootDSE -Credential $Credential
-$SubCA = $AdcsObject | Where-Object Name -eq SubCA
+$results = Invoke-Locksmith2 -Forest $Forest -Credential $Credential -Verbose
