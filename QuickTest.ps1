65e54144d92a9d34641414cb625b56a19fcd85b5
Jake Hildreth <jakehildreth@gmail.com>
2025-11-30 06:04:22 -0800

    chore: updated to reflect function rename

--- QuickTest.ps1
+++ QuickTest.ps1
@@ -2,4 +2,5 @@ Set-Location ~\Documents\Locksmith2\
 Import-Module .\Locksmith2.psd1 -Force
 $Credential = New-Credential -User 'adcs.goat\Administrator'
 $Forest = 'adcs.goat'
-$results = Invoke-Locksmith2 -Forest $Forest -Credential $Credential -Verbose
+$results = Invoke-Locksmith2 -Forest $Forest -Credential $Credential -Verbose -SkipPowerShellCheck
+$stores = Get-Locksmith2Stores
